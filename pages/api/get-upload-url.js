78aa2a3dd40ea6d533c5253c09da60b84e52c726
Said Abdul Aziem <cairocoder@Saids-MacBook-Pro.local>
2025-06-29 11:59:02 +0300

    Ready for Vercel deployment

--- pages/api/get-upload-url.js
+++ pages/api/get-upload-url.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]";
+import { authOptions } from "./auth/[...nextauth]";
 import clientPromise from "../../lib/mongodb";
 
 const b2cs = require("b2-cloud-storage");
